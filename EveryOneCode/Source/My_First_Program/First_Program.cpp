af25a7c78a0928ebc56585de6ba4061daec8d034
SongKyun <89376275+SongKyun@users.noreply.github.com>
2025-06-06 22:37:39 +0900

    레퍼런스
    Signed-off-by: SongKyun <89376275+SongKyun@users.noreply.github.com>

--- EveryOneCode/Source/My_First_Program/First_Program.cpp
+++ EveryOneCode/Source/My_First_Program/First_Program.cpp
@@ -2,9 +2,22 @@
 
 #include <iostream>
 
+int change_val(int &p)
+{
+    p = 3;
+    return 0;
+}
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    /*int a = 3;
+    int& another_a = a;
+
+    another_a = 5;
+    std::cout<< "a : " << a << std::endl;
+    std::cout << "another_a : " << another_a << std::endl;*/
+    
+    /*std::cout << "Hello World!" << std::endl;
     std::cout <<"hi" << std::endl << "my name is" << "Psi" << std::endl;
-    return 0;
+    return 0;*/
 }
